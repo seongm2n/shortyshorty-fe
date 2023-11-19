bbbaf10274e6ca4c6b27e4ad7defb0b982159582
seongmin <seongm22n@gmail.com>
2023-11-19 21:19:10 +0900

    Style :  기능 설정 전 기본 스타일링

---
+++ src/components/main.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+const InputWrapper = styled.div`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Input = styled.input`
+	padding: 8px;
+	border: 1px solid #ccc;
+	border-radius: 20px;
+	width: 534px;
+	height: 60px;
+	font-size: 20px;
+`;
+
+const Button = styled.button`
+	position: absolute;
+	top: 50%;
+	transform: translateY(-50%);
+	right: 0;
+	padding: 8px;
+	margin-right: 10px;
+	background-color: #d9967e;
+	color: #f2f2eb;
+	border: 1px solid #d9967e;
+	border-radius: 15px;
+	font-size: 16px;
+	cursor: pointer;
+
+	&:hover {
+		background-color: #b5645a;
+	}
+`;
+
+export default function Main() {
+	return (
+		<Wrapper>
+			<InputWrapper>
+				<Input
+					type='text'
+					placeholder='Please enter a long URL...'
+				/>
+				<Button>short</Button>
+			</InputWrapper>
+		</Wrapper>
+	);
+}
