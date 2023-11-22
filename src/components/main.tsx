79f29e8f68c11169484009a16908a16ab4eb0a6d
seongmin <seongm22n@gmail.com>
2023-11-22 19:39:17 +0900

    Feat : copy delete 버튼 추가
    latest url 보이는 기능, copy 버튼 추가
    historylist 추가 -> blur처리때문에 copy,delete버튼이 작동 안됨(해결해야함)

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,3 +1,4 @@
+import { SetStateAction, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -19,6 +20,7 @@ const Input = styled.input`
 	width: 534px;
 	height: 60px;
 	font-size: 20px;
+	outline: none;
 
 	@media (max-width: 768px) {
 		width: 367px;
@@ -48,16 +50,176 @@ const Button = styled.button`
 	}
 `;
 
+const HistoryList = styled.ul`
+	position: absolute;
+	top: 100%;
+	left: 0;
+	width: 100%;
+	list-style: none;
+	padding: 10px;
+	border: 1px solid #ccc;
+	border-radius: 0 0 20px 20px;
+	background-color: #fff;
+	z-index: 1;
+`;
+
+const HistoryItem = styled.li`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	text-align: center;
+	align-items: center;
+	margin-bottom: 5px;
+	position: relative;
+	z-index: 1;
+`;
+
+const CopyButton = styled.button`
+	background-color: #fffcfc;
+	color: #d9967e;
+	border: 1px solid #d9967e;
+	border-radius: 20px;
+	padding: 8px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	margin-right: 5px;
+	cursor: pointer;
+`;
+
+const DeleteButton = styled.button`
+	background-color: #fffcfc;
+	color: #d9967e;
+	border: 1px solid #d9967e;
+	border-radius: 20px;
+	padding: 8px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	cursor: pointer;
+`;
+
+const LatestShortURL = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	text-align: center;
+	align-items: center;
+	padding: 15px;
+	width: 534px;
+	height: 60px;
+	font-size: 20px;
+
+	@media (max-width: 768px) {
+		width: 367px;
+		font-size: 17px;
+	}
+`;
+
 export default function Main() {
+	const [inputValue, setInputValue] = useState('');
+	const [historyList, setHistoryList] = useState<string[]>([]);
+	const [isInputFocused, setIsInputFocused] = useState(false);
+	const [latestShortURL, setLatestShortURL] = useState<string>('');
+	const [isHistoryVisible, setIsHistoryVisible] = useState(false);
+
+	const inputChange = (e: { target: { value: SetStateAction<string> } }) => {
+		setInputValue(e.target.value);
+	};
+
+	const buttonClick = (e: React.FormEvent) => {
+		e.preventDefault();
+		if (inputValue.trim() !== '') {
+			setHistoryList((prevList) => [...prevList, inputValue]);
+			setInputValue('');
+			setLatestShortURL(inputValue);
+		}
+	};
+
+	const inputFocus = () => {
+		setIsInputFocused(true);
+	};
+
+	const inputBlur = () => {
+		setIsInputFocused(false);
+		if (!isHistoryVisible) {
+			setIsHistoryVisible(false);
+		}
+	};
+
+	const handleCopy = (url: string) => {
+		console.log('Copy button clicked');
+		console.log('Copied:', url);
+	};
+
+	const handleDelete = (index: number) => {
+		console.log('Delete button clicked');
+		const updatedList = [...historyList];
+		updatedList.splice(index, 1);
+		setHistoryList(updatedList);
+	};
+
+	useEffect(() => {
+		console.log('History List Updated:', historyList);
+	}, [historyList]);
+
 	return (
 		<Wrapper>
-			<InputWrapper>
-				<Input
-					type='text'
-					placeholder='Please enter a long URL...'
-				/>
-				<Button>short</Button>
-			</InputWrapper>
+			<form onSubmit={buttonClick}>
+				<InputWrapper>
+					<Input
+						type='text'
+						placeholder='Please enter a long URL...'
+						value={inputValue}
+						onChange={inputChange}
+						onFocus={inputFocus}
+						onBlur={inputBlur}
+						style={{
+							borderRadius:
+								isInputFocused && historyList.length > 0
+									? '20px 20px 0 0'
+									: '20px',
+							borderBottom:
+								isInputFocused && historyList.length > 0
+									? 'none'
+									: '1px solid #ccc',
+						}}
+					/>
+
+					<Button type='submit'>short</Button>
+
+					{isInputFocused && isHistoryVisible && historyList.length > 0 && (
+						<HistoryList>
+							{historyList.map((item, index) => (
+								<HistoryItem key={index}>
+									<span>{item}</span>
+									<div>
+										<CopyButton onClick={() => handleCopy(item)}>
+											Copy
+										</CopyButton>
+										<DeleteButton onClick={() => handleDelete(index)}>
+											Delete
+										</DeleteButton>
+									</div>
+								</HistoryItem>
+							))}
+						</HistoryList>
+					)}
+				</InputWrapper>
+			</form>
+			<LatestShortURL>
+				<span>{latestShortURL}</span>
+
+				{latestShortURL ? (
+					<CopyButton onClick={() => handleCopy(latestShortURL)}>
+						Copy
+					</CopyButton>
+				) : (
+					''
+				)}
+			</LatestShortURL>
 		</Wrapper>
 	);
 }
