1fdd5ee4b9c96e94d0693d7c6a8a58f1fbff156f
seongmin <seongm22n@gmail.com>
2023-11-22 19:37:34 +0900

    Fix : 페이지별 header 다르게수정
    styled-components대신 jsx에 상태 추가해서 수정

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Wrapper = styled.div`
 	display: flex;
@@ -17,20 +17,34 @@ const Wrapper = styled.div`
 	}
 `;
 
-const Header = styled.div<{ aboutPage: boolean }>`
+const headerStyles = css`
 	display: flex;
 	justify-content: center;
 	width: 100%;
-	font-size: ${(props) => (props.aboutPage ? '30px' : '60px')};
+	font-size: 60px;
 	font-weight: 300;
 	color: #bf533b;
 	line-height: normal;
 	text-decoration: none;
-	padding-top: ${(props) => (props.aboutPage ? '10px' : '100px')};
-	
+	padding-top: 100px;
+
 	@media (min-width: 768px) {
-		font-size: ${(props) => (props.aboutPage ? '40px' : '80px')};
-		padding-top: ${(props) => (props.aboutPage ? '20px' : '200px')};
+		font-size: 80px;
+		padding-top: 200px;
+	}
+`;
+
+const Header = styled.div`
+	${headerStyles}
+
+	&.about-page-header {
+		font-size: 30px;
+		padding-top: 10px;
+
+		@media (min-width: 768px) {
+			font-size: 40px;
+			padding-top: 20px;
+		}
 	}
 `;
 
@@ -60,7 +74,7 @@ export default function Layout() {
 
 	return (
 		<Wrapper>
-			<Header aboutPage={aboutPage}>
+			<Header className={aboutPage ? 'about-page-header' : ''}>
 				<Link to='/'>shortyshorty</Link>
 			</Header>
 			<Outlet />
