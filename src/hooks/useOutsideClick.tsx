17c555c9c16c1ba3588390c7a86fe2aee4f4059c
seongmin <seongm22n@gmail.com>
2023-12-01 20:03:29 +0900

    Feat : outsideClick 훅 추가
    historylist 이외의 공간을 누르면 접히는 훅

---
+++ src/hooks/useOutsideClick.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useCallback, RefObject } from 'react';
+
+type OutsideClickHandler = (event: MouseEvent) => void;
+
+interface UseOutsideClickProps {
+  ref: RefObject<HTMLElement>;
+  handler: OutsideClickHandler;
+}
+
+export function useOutsideClick({ ref, handler }: UseOutsideClickProps) {
+  const handleClick = useCallback(
+    (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        handler(e);
+      }
+    },
+    [ref, handler],
+  );
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, [handleClick]);
+}
