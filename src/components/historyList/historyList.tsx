6ea31339c30e61eb007cb22c3e1a34a4237b6c32
seongmin <seongm22n@gmail.com>
2023-12-06 21:36:59 +0900

    Fix : historyLIst 분기 나눔

---
+++ src/components/historyList/historyList.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components';
+import { CopyButton, DeleteButton } from '../../styles/button';
+
+const HistoryListStyle = styled.ul`
+	position: absolute;
+	top: 100%;
+	left: 0;
+	width: 100%;
+	list-style: none;
+	padding: 10px;
+	border: 1px solid #ccc;
+	border-radius: 0 0 20px 20px;
+	background-color: #fff;
+	z-index: 1;
+`;
+
+const HistoryItemStyle = styled.li`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	text-align: center;
+	align-items: center;
+	margin-bottom: 5px;
+	position: relative;
+	z-index: 1;
+	cursor: pointer;
+`;
+
+interface HistoryListProps {
+	historyList: string[];
+	handleCopyUrl: (url: string) => void;
+	handleDelete: (index: number) => void;
+	isCopied: boolean;
+}
+
+const HistoryList: React.FC<HistoryListProps> = ({
+	historyList,
+	handleCopyUrl,
+	handleDelete,
+	isCopied,
+}) => {
+	return (
+		<div>
+			{historyList.length > 0 && (
+				<HistoryListStyle>
+					{historyList.map((item, index) => (
+						<HistoryItemStyle key={index}>
+							<span>{item}</span>
+							<div>
+								<CopyButton
+									onClick={() => handleCopyUrl(item)}
+									disabled={isCopied}
+								>
+									Copy
+								</CopyButton>
+								<DeleteButton onClick={() => handleDelete(index)}>
+									Delete
+								</DeleteButton>
+							</div>
+						</HistoryItemStyle>
+					))}
+				</HistoryListStyle>
+			)}
+		</div>
+	);
+};
+
+export default HistoryList;
