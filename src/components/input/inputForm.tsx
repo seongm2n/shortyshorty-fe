c202128898036c4aac03514894fffd4d19a7abff
seongmin <seongm22n@gmail.com>
2023-12-06 21:37:49 +0900

    FIx : inputForm 분기 나눔

---
+++ src/components/input/inputForm.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Input } from '../../styles/input';
+
+interface InputFormProps {
+	inputValue: string;
+	onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	onFocus: () => void;
+	isSearchOpen: boolean;
+	historyList: string[];
+}
+
+const InputForm: React.FC<InputFormProps> = ({
+	inputValue,
+	onInputChange,
+	onFocus,
+	isSearchOpen,
+	historyList,
+}) => {
+	return (
+		<Input
+			type='text'
+			placeholder='Please enter a long URL...'
+			value={inputValue}
+			onChange={onInputChange}
+			onFocus={onFocus}
+			style={{
+				borderRadius:
+					isSearchOpen && historyList.length > 0 ? '20px 20px 0 0' : '20px',
+				borderBottom:
+					isSearchOpen && historyList.length > 0 ? 'none' : '1px solid #ccc',
+			}}
+		/>
+	);
+};
+
+export default InputForm;
