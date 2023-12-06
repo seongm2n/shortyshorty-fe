23ae2a2a49f63c112b1ae93d3e45a2de758e90e6
seongmin <seongm22n@gmail.com>
2023-12-06 21:39:08 +0900

    Style : button, input 스타일 나눔

---
+++ src/styles/input.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+	padding: 10px;
+	border: 1px solid #ccc;
+	border-radius: 20px;
+	width: 534px;
+	height: 60px;
+	font-size: 20px;
+	outline: none;
+	cursor: pointer;
+
+	@media (max-width: 768px) {
+		width: 367px;
+		font-size: 17px;
+	}
+`;
+
+export const InputWrapper = styled.form`
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
