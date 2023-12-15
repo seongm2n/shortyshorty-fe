550496571ab65a2a39c2d505e80e4b6316033e20
seongmin <seongm22n@gmail.com>
2023-12-15 14:56:53 +0900

    Fix : .

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
 	baseURL: 'https://api.shortyshorty.site',
 	headers: {
-		'Access-Control-Allow-Origin': '*',
+		// 'Access-Control-Allow-Origin': '*',
 		'Content-Type': 'application/json',
 	},
 	timeout: 10000,
