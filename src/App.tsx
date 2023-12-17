cdb062d599559c55b948bf877fe3db7d291176c0
seongmin <seongm22n@gmail.com>
2023-12-18 03:17:59 +0900

    Fix: basename

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
 	return (
 		<Wrapper>
 			<GlobalStyled />
-			<Router basename={process.env.PUBLIC_URL}>
+			<Router basename='/shortyshorty-fe'>
 				<Routes>
 					<Route
 						path='/'
