74d48afbe146bcf6ccba2d454b037f4c3e46072b
seongmin <seongm22n@gmail.com>
2023-12-18 05:50:43 +0900

    fix : redirect loading  부분

--- src/components/redirect.tsx
+++ src/components/redirect.tsx
@@ -1,32 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getApi } from '../config/axios';
 
 const RedirectComponent = () => {
 	const { key } = useParams<{ key?: string }>();
-	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const fetchRedirectData = async () => {
 			try {
-        if (key) {
-          const originalUrl = await getApi(key);
-          console.log(originalUrl);
+				if (key) {
+					const originalUrl = await getApi(key);
 
-          window.location.href = originalUrl;
+					window.location.href = originalUrl;
 				} else {
 					console.error('Key is undefined or null');
 				}
 			} catch (error) {
 				console.error('Failed to get original URL', error);
-			} finally {
-				setLoading(false);
 			}
 		};
 		fetchRedirectData();
 	}, [key]);
 
-	return <div>{loading && <p>Loading...</p>}</div>;
+	return null;
 };
 
 export default RedirectComponent;
