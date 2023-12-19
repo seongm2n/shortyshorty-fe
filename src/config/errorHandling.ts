715f68af5313048f87c7b362e438575f7128b8f2
seongmin <seongm22n@gmail.com>
2023-12-19 13:22:07 +0900

    Feat: 500 에러 추가

--- src/config/errorHandling.ts
+++ src/config/errorHandling.ts
@@ -3,8 +3,11 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export const apiError = (error: any) => {
+	console.error('API 에러:', error); // 추가된 부분
 	if (is502Error(error)) {
-		toast.error('502 데이터베이스 고치는중~');
+		toast.error('502 Bad Gateway');
+	} else if (is500Error(error)) {
+		toast.error('500 Internal Server Error');
 	} else if (is404Error(error)) {
 		toast.error('404 페이지를 찾을 수 없어요!');
 	} else if (isTimeoutError(error)) {
@@ -18,10 +21,14 @@ export const is404Error = (error: any): boolean => {
 	return error.isAxiosError && (error as AxiosError).response?.status === 404;
 };
 
-export const isTimeoutError = (error: any): boolean => {
-	return error.isAxiosError && error.code === 'ECONNABORTED';
+export const is500Error = (error: any): boolean => {
+	return error.isAxiosError && (error as AxiosError).response?.status === 500;
 };
 
 export const is502Error = (error: any): boolean => {
 	return error.isAxiosError && (error as AxiosError).response?.status === 502;
+};
+
+export const isTimeoutError = (error: any): boolean => {
+	return error.isAxiosError && error.code === 'ECONNABORTED';
 };
