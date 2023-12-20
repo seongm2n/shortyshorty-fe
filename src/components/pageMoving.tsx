87c67fc84c84fb29926570aaf69a0c2d26ca488c
seongmin <seongm22n@gmail.com>
2023-12-20 16:38:19 +0900

    Style: center 맞추기

--- src/components/pageMoving.tsx
+++ src/components/pageMoving.tsx
@@ -2,30 +2,34 @@ import { PropagateLoader } from 'react-spinners';
 import styled from 'styled-components';
 
 const PageLoadingWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 70vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 70vh;
 
-  p {
-    margin: 15px 0;
-    font-size: 18px;
-    color:#51474e;
-  }
+	p {
+		margin: 15px 0;
+		font-size: 18px;
+		color: #51474e;
+	}
 
-  .loader {
-    margin-top: 15px; /* Adjust the margin-top as needed */
-  }
-
+	.loader {
+		margin-top: 15px;
+		align-items: center;
+		justify-content: center;
+	}
 `;
 
 export default function PageMoving() {
-  return (
-    <PageLoadingWrapper>
-      <p>잠시만 기다려주세요</p>
-      <p>페이지 이동 중</p>
-      <PropagateLoader className="loader" color='#d9967e' />
-    </PageLoadingWrapper>
-  );
+	return (
+		<PageLoadingWrapper>
+			<p>잠시만 기다려주세요</p>
+			<p>페이지 이동 중</p>
+			<PropagateLoader
+				className='loader'
+				color='#d9967e'
+			/>
+		</PageLoadingWrapper>
+	);
 }
